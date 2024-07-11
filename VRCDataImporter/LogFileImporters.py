151710bdbe4bc679042002fb911e756115edfbec
Rami-Pastrami <rami@ramipastrami.engineering>
2024-07-11 00:23:46 -0400

    write out example

--- VRCDataImporter/LogFileImporters.py
+++ VRCDataImporter/LogFileImporters.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from enum import Enum
 
 # Following Settings affect parsing of log data.
 # Ensure this matches the export settings Unity side
@@ -9,15 +10,67 @@ HEADER_CSV: str = "CSV"
 HEADER_2DCSV: str = "2D_CSV"
 HEADER_JSON: str = "JSON"
 
+class DATA_PARTITION(Enum):
+    FULL: int = 0
+    START: int = 1
+    MIDDLE: int = 2
+    END: int = 3
+    DYNAMIC: int = 4
+
+class HEADER_TYPE(Enum):
+    HEADER_STRING: int = 0
+    HEADER_CSV: int = 1
+    HEADER_2DCSV: int = 2
+    HEADER_JSON: int = 3
+
+class RawLogLine:
+    def __init__(self, line: str):
+        if not RawLogLine.is_line_export_line(line):
+            raise Exception("This line does not seem to be a log exported line!")
+        isolated_line: str = line.split( EXPORT_IDENTIFIER)[1]
+        components: list[str] = isolated_line.split(EXPORT_SEPERATOR)
+        self.tag: str = components[0]
+        self.partition_type: DATA_PARTITION = DATA_PARTITION(components[1])
+        self.data_type: HEADER_TYPE = HEADER_TYPE(components[2])
+        self.raw_string: str = components[3]
+        self.processed_value = None
+        match self.data_type:
+            case HEADER_TYPE.HEADER_CSV:
+                self.processed_value = self._process_string_as_csv(self.raw_string)
+            case _:
+                raise NotImplementedError
+
+    @staticmethod
+    def is_line_export_line(line: str) -> bool:
+        return EXPORT_IDENTIFIER in line
+
+
+    def _process_string_as_csv(self, raw_string: str) -> list:
+        elements: list = raw_string.split(",")
+        ## Try converting to float, then int, then leave as string
+        for i in range(len(elements)):
+            try:
+                cache = float(elements[i])
+                elements[i] = cache
+                continue
+            except:
+                try:
+                    cache = int(elements[i])
+                    elements[i] = cache
+                    continue
+                except:
+                    continue
+        return elements
+
 
 def read_log_file_without_further_formatting(log_file_path: Path):
     f = open(log_file_path, 'r', encoding='utf8')
-    output: list[LogFileObjects.RawLogLine] = []
+    output: list[ RawLogLine] = []
     for line in f:
         line_stripped: str = line.strip()
-        if not LogFileObjects.RawLogLine.is_line_export_line(line_stripped):
+        if not RawLogLine.is_line_export_line(line_stripped):
             continue
-        output.append(LogFileObjects.RawLogLine(line_stripped))
+        output.append(RawLogLine(line_stripped))
 
 
 
--- example.py
+++ example.py
@@ -1,6 +1,10 @@
 from pathlib import Path
-from VRCDataImporter.Importers import LogFileImporters
 
-current_path: Path = __file__
-print(current_path)
+current_path: Path = Path(__file__)
+current_folder: Path = current_path.parent
+file_name: Path = Path("example_log.txt")
+example_log_file_path: Path = current_folder.joinpath(file_name)
+
+
+
 
