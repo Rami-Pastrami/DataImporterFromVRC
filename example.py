62a6057d57c80de65af66da5cbe023074e21541d
Rami-Pastrami <rami@ramipastrami.engineering>
2024-08-08 01:48:28 -0400

    update example with new definition

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pandas as pd
-from VRCDataImporter.LogFileImporters import read_log_file_without_further_formatting, segregate_by_tag_to_dict, segregate_by_tag_to_dataframe_arrays, RawLogLine
+from VRCDataImporter import read_log_file_without_further_formatting, segregate_by_tag_to_dict, segregate_by_tag_to_dataframe_arrays, RawLogLine
 
 # This just gets the example file path
 current_path: Path = Path(__file__)
@@ -12,6 +12,6 @@ example_log_file_path: Path = current_folder.joinpath(file_name)
 raw_line_objects: list[RawLogLine] = read_log_file_without_further_formatting(example_log_file_path)
 filtered: dict = segregate_by_tag_to_dict(raw_line_objects)
 data_frame: pd.DataFrame = segregate_by_tag_to_dataframe_arrays(raw_line_objects, "color", ["X", "Y", "Z"] )
-
+print(data_frame)
 
 print("Program End") # easy breakpoint
